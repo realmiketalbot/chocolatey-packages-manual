b81e220088fb85fd35591a54d8f4a97ddd5308b6
Mike Talbot <29737293+realmiketalbot@users.noreply.github.com>
2021-10-08 15:55:56 -0500

    Add SWMM 5.1.015

---
+++ swmm/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
+# Internal/Organization: Download from internal location (internet sources are unreliable)
+$url        = 'https://www.epa.gov/sites/default/files/2020-08/swmm51015_setup.exe' # download url, HTTPS preferred
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'swmm*'
+
+  checksum      = '69D89C75168A689ABCC9F754E2EF5E20BDA978BE4AA682DCC90A444A4A91835B'
+  checksumType  = 'sha256'
+
+  PackageVersion= '5.1.015'
+  MaintainerName= 'Mike Talbot'
+
+  silentArgs   = '/s /v"/qn"' 
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
