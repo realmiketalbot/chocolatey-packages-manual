eeef0299c934dbb184b92a919623685e206b1ee6
Mike Talbot <29737293+realmiketalbot@users.noreply.github.com>
2021-10-08 15:36:36 -0500

    Add HEC-HMS 4.8

---
+++ hec-hms/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
+# Internal/Organization: Download from internal location (internet sources are unreliable)
+$url        = 'https://github.com/HydrologicEngineeringCenter/hec-downloads/releases/download/1.0.19/HEC-HMS_48_Setup.exe' # download url, HTTPS preferred
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'hec-hms*'
+
+  checksum      = 'BA14BDC3684E963CFAA5694FE4EA4D09E6E439ABB36AB4DAE671F8AC70B7C4F1'
+  checksumType  = 'sha256'
+
+  PackageVersion= '4.8'
+  MaintainerName= 'Mike Talbot'
+
+  silentArgs   = '/s /v"/qn"' 
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
