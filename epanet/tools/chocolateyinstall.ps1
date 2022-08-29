0b42ce1230a3158e0d78980d7bbba1b1a4338f55
Mike Talbot <29737293+realmiketalbot@users.noreply.github.com>
2022-08-29 14:55:22 -0600

    Add EPANET package

---
+++ epanet/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
+# Internal/Organization: Download from internal location (internet sources are unreliable)
+$url        = 'https://github.com/USEPA/EPANET2.2/releases/download/2.2.0/epanet2.2_setup.exe' # download url, HTTPS preferred
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'epanet*'
+
+  checksum      = '79D9A585A557D13D8471D713D0AE093D9CCA67C14B8E573223D71EEE8B89F671'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/s /v"/qn"' 
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
