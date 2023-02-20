f4c43cd58fd5d7f92b8e9e54b96feedfaccf8074
realmiketalbot <29737293+realmiketalbot@users.noreply.github.com>
2023-02-20 12:25:29 -0700

    Update HEC-RAS to version 6.3.1

--- hec-ras/tools/chocolateyinstall.ps1
+++ hec-ras/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
 # Internal/Organization: Download from internal location (internet sources are unreliable)
-$url        = 'https://github.com/HydrologicEngineeringCenter/hec-downloads/releases/download/1.0.25/HEC-RAS_63_Setup.exe' # download url, HTTPS preferred
+$url        = 'https://github.com/HydrologicEngineeringCenter/hec-downloads/releases/download/1.0.26/HEC-RAS_631_Setup.exe' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'hec-ras*'
 
-  checksum      = 'F7AD7417A853FF9C57BAA65D087EB0C5103B8FF8BCC7CE4D3F24AB6FB7DFE0C8'
+  checksum      = '5DE0BD85B61DA6D4ED450D443EC1D4BF4BD30D309496C7DD55A6B7011784D827'
   checksumType  = 'sha256'
 
   silentArgs   = '/s /v"/qn"' 
