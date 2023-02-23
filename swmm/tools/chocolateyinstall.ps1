ed00de000ebb9421d8e8917c5e601be27c2af7f5
realmiketalbot <29737293+realmiketalbot@users.noreply.github.com>
2023-02-22 19:15:13 -0700

    Updates

--- swmm/tools/chocolateyinstall.ps1
+++ swmm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
 # Internal/Organization: Download from internal location (internet sources are unreliable)
-$url        = 'https://www.epa.gov/system/files/other-files/2022-08/Self-Extracting%20Installation%20Program%20for%20SWMM%205.2.1%20%2864-bit%29%C2%A0.exe' # download url, HTTPS preferred
+$url        = 'https://www.epa.gov/system/files/other-files/2022-12/swmm522%28x64%29_setup.exe' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'swmm*'
 
-  checksum      = 'E1E77F5948D0277B8D0AFDE48E5C99A86A1E1A48DE24A3CAF254C0B2DD976550'
+  checksum      = '92E8380DB3B10B67707A9769BFFC486F67795030729683FECA25A9DD2205BC44'
   checksumType  = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
