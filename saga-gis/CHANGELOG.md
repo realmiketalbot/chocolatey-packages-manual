bbb51275dc81c01a042f9a208eaa83e38775b7a3
realmiketalbot <29737293+realmiketalbot@users.noreply.github.com>
2023-02-28 20:03:02 -0700

    Add SAGA version 8.5.1

--- saga-gis/CHANGELOG.md
+++ saga-gis/CHANGELOG.md
@@ -22,3 +22,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Version 7.9.1 installer
+
+## [8.5.1] - 2023-03-01
+
+### Added
+
+- Version 8.5.1 installer
--- saga-gis/tools/chocolateyinstall.ps1
+++ saga-gis/tools/chocolateyinstall.ps1
@@ -1,16 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://pilotfiber.dl.sourceforge.net/project/saga-gis/SAGA%20-%207/SAGA%20-%207.9.1/saga-7.9.1_x64_setup.exe'
+$url        = 'https://gigenet.dl.sourceforge.net/project/saga-gis/SAGA%20-%208/SAGA%20-%208.5.1/saga-8.5.1_win32_setup.exe'
+$url64      = 'https://cytranet.dl.sourceforge.net/project/saga-gis/SAGA%20-%208/SAGA%20-%208.5.1/saga-8.5.1_x64_setup.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = $url
 
   softwareName  = 'saga-gis*'
 
-  checksum      = '86433A626DC6316B4EA4B275CB7A5D2991EE018056B5997925D8702EB421F827'
+  Url           = $url
+  checksum      = '3F430F9871D485E6D8405C0FD1DB33E9305888DDF97511A3DC1D6A5BFF098609'
   checksumType  = 'sha256'
+
+  Url64bit      = $url64
+  checksum64    = '40E44CD404470BB45C943A387C81B7B012F387A7266A64C49A1687C847FDAC78'
+  checksumType64= 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0, 3010, 1641)
