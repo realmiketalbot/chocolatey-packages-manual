5e1d285fc5eaad426b6d207bde2a94a41cae781c
realmiketalbot <29737293+realmiketalbot@users.noreply.github.com>
2026-01-03 21:48:29 +0000

    netextender: update to latest upstream release

--- netextender/tools/chocolateyinstall.ps1
+++ netextender/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # downloads available on this page: https://www.sonicwall.com/products/remote-access/vpn-clients/
-$url        = 'https://software.sonicwall.com/NetExtender/NetExtender-x86-10.3.1.msi'
-$url64bit      = 'https://software.sonicwall.com/NetExtender/NetExtender-x64-10.3.1.msi'
+$url        = 'https://software.sonicwall.com/NetExtender/NetExtender-x86-10.3.2.msi'
+$url64bit      = 'https://software.sonicwall.com/NetExtender/NetExtender-x64-10.3.2.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,11 +11,11 @@ $packageArgs = @{
   softwareName  = 'netextender*'
 
   url           = $url
-  checksum      = '25EE3E4FC42E7ACD0014D0670E575535045F7446171DF02F2389EC75D56AF5E7'
+  checksum      = '566FD2DDCC853DD9E96A39922AB1C4EEB685081C74112E4B644957F67DDECE29'
   checksumType  = 'sha256'
 
   url64bit      = $url64bit
-  checksum64    = '5053CDED98D5213A5F5959A9695FF95E12A664AC271EA5B0122D3CA8B087521C'
+  checksum64    = '793E2ED53A2D7457F387C36F2828177B63FE120DFB2B27F824BB457B78EC8AB1'
   checksumType64= 'sha256'
 
 
